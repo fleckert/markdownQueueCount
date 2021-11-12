1f00eb29ac7c961e28170eb3d6149630452dc719
Florian Eckert <fleckert@microsoft.com>
2021-11-12 06:18:27 +0100

    init

---
+++ httpTrigger/index.ts
@@ -0,0 +1,38 @@
+
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { EOL                                 } from "os";
+import { QueueClient, QueueServiceClient     } from "@azure/storage-queue";
+import { DefaultAzureCredential              } from "@azure/identity";
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+
+    const storageAccountName = "<<fill_in>>";
+    const url = `https://${storageAccountName}.queue.core.windows.net`;
+    const credential = new DefaultAzureCredential();
+
+    const queueServiceClient = new QueueServiceClient(url, credential);
+
+    const queues = queueServiceClient.listQueues();
+
+    let markdown = `|name|approximateMessagesCount|${EOL}`;
+
+    for await (const queue of queues) {
+        const queueClient = new QueueClient(`${url}/${queue.name}`, credential);
+
+        const queueProperties = await queueClient.getProperties();
+
+        markdown += `|${queue.name}|${queueProperties.approximateMessagesCount}|${EOL}`;
+    }
+
+    context.res = {
+        status: 200,
+        headers: {
+            "Content-Type": "text/plain; charset=utf-8",
+            "Access-Control-Allow-Origin": "*",
+            "Cache-Control": "max-age=0"
+        },
+        body: markdown
+    };
+};
+
+export default httpTrigger;
